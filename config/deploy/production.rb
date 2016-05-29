969080010e5e42de5b0600690ff3693ceecdc126
Vasile Buza <vasilebuza63@gmail.com>
2016-05-29 14:45:19 +0300

    add use sudo false

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -9,6 +9,7 @@ server '23.92.65.226', port: 22, roles: [:web, :app, :db], primary: true
 set :application, 'HsctmTest'
 set :repo_url, 'git@github.com:vasile-buza/hsctm-test.git'
 set :stage, :production
+set :use_sudo, false
 
 set :deploy_to, '/home/hiscore/hsctm_test'
 set :scm, :git
