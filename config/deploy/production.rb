a227d67a84a0e18e288af96d56973c310f38ddbe
Vasile Buza <vasilebuza63@gmail.com>
2016-05-29 14:08:48 +0300

    add production crediantials

---
+++ config/deploy/production.rb
@@ -0,0 +1,16 @@
+set :ssh_options, {
+    keys: %w(/home/vasile/RubymineProjects/hsctm_test/tmp/vps),
+    forward_agent: true,
+    user: 'hiscore'
+}
+
+server '23.92.65.226', port: 22, roles: [:web, :app, :db], primary: true
+
+set :application, 'HsctmTest'
+set :repository, 'git@github.com:vasile-buza/hsctm-test.git'
+
+set :deploy_to, '/home/hiscore/hsctm_test'
+set :scm, :git
+set :branch, 'master'
+set :user, 'hiscore'
+
